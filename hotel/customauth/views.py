2fa3dfb2b45a2bf3cb31a0ad97024fb755704f99
NaifZaghmout zaghmout <naif-zaghmout@hotmail.com>
2023-10-13 15:14:34 +0000

    Added staff registration and success message handling in 'customauth/views.py'.

--- hotel/customauth/views.py
+++ hotel/customauth/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
 import datetime
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
@@ -23,7 +24,7 @@ def user_sign_up(request):
     """
     User Sign Up page
     """
-    
+
     if request.method != "POST":
         return HttpResponse("Access Denied")
 
@@ -49,3 +50,38 @@ def user_sign_up(request):
     new_user.save()
     messages.success(request, "Registration Successfull")
     return redirect("userloginpage")
+
+
+
+
+
+def staff_sign_up(request):
+
+    """
+    Staff Sign up page
+    """
+
+    if request.method != "POST":
+        return HttpResponse("Access Denied")
+
+    user_name = request.POST["username"]
+
+    password1 = request.POST["password1"]
+    password2 = request.POST["password2"]
+
+    if password1 != password2:
+        messages.success(request, "Password didn't Matched")
+        return redirect("staffloginpage")
+    try:
+        if User.objects.all().get(username=user_name):
+            messages.warning(request, "Username Already Exist")
+            return redirect("staffloginpage")
+    except:
+        pass
+
+    new_user = User.objects.create_user(username=user_name, password=password1)
+    new_user.is_superuser = False
+    new_user.is_staff = True
+    new_user.save()
+    messages.success(request, " Staff Registration Successfull")
+    return redirect("staffloginpage")
