d49932d2c7fda8610f1b760faa5d29bc1607daa3
NaifZaghmout zaghmout <naif-zaghmout@hotmail.com>
2023-10-19 09:55:22 +0000

    Tested adding a new location with a staff user in 'management/tests.py

--- hotel/management/tests.py
+++ hotel/management/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth.models import User
+from app.models import Hotels, Rooms
 
-# Create your tests here.
+
+class StaffViewTestCase(TestCase):
+    # ...[setUp and tearDown methods]...
+
+    def test_add_new_location_with_staff_user(self):
+        self.client.login(username="staffuser", password="testpassword")
+        response = self.client.post(
+            reverse("addnewlocation"),
+            {
+                "new_owner": "owner2",
+                "new_city": "city2",
+                "new_state": "state2",
+                "new_country": "country2",
+            },
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(Hotels.objects.filter(hotel_location="city2").exists())
