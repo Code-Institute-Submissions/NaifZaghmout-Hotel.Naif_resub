04223b04aacb26235b538e2af2a1c24c6b975104
NaifZaghmout zaghmout <naif-zaghmout@hotmail.com>
2023-10-20 13:04:41 +0000

    Registered models , app/admin.py

--- hotel/app/admin.py
+++ hotel/app/admin.py
@@ -1,3 +1,12 @@
+
+"""
+Admin
+"""
 from django.contrib import admin
+from .models import Hotels, Reservation, Rooms
 
 # Register your models here.
+
+admin.site.register(Hotels)
+admin.site.register(Reservation)
+admin.site.register(Rooms)
