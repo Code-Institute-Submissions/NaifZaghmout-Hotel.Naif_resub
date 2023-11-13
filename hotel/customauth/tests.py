d71d1da550fac6781756c62542566603e3781c66
NaifZaghmout zaghmout <naif-zaghmout@hotmail.com>
2023-11-13 13:21:23 +0000

    update my test.py files  and managment urls.py

--- hotel/customauth/tests.py
+++ hotel/customauth/tests.py
@@ -1,85 +1,91 @@
 from django.test import TestCase
-
-# Create your tests here.
-
-
-from django.test import TestCase, Client
 from django.urls import reverse
 from django.contrib.auth.models import User
 
 
-class UserViewsTestCase(TestCase):
+class CustomAuthViewsTestCase(TestCase):
     def setUp(self):
-        self.client = Client()
-        self.username = "testuser"
-        self.password = "testpassword"
+        # Create a test user
+        self.test_user = User.objects.create_user(
+            username="testuser", password="testpassword"
+        )
 
-    def test_user_sign_up(self):
+    def test_user_sign_up_view(self):
         response = self.client.post(
             reverse("usersignup"),
             {
-                "username": self.username,
-                "password1": self.password,
-                "password2": self.password,
+                "username": "newuser",
+                "password1": "testpassword",
+                "password2": "testpassword",
             },
         )
-        self.assertEqual(response.status_code, 302)  # expecting a redirect
-        self.assertTrue(User.objects.filter(username=self.username).exists())
 
-    def test_staff_sign_up(self):
+        self.assertEqual(
+            response.status_code, 302
+        )  # Check if the redirect is successful
+
+        # Check if the user was created and logged in
+        new_user = User.objects.get(username="newuser")
+        self.assertTrue(new_user.is_authenticated)
+
+    def test_staff_sign_up_view(self):
         response = self.client.post(
             reverse("staffsignup"),
             {
-                "username": self.username,
-                "password1": self.password,
-                "password2": self.password,
+                "username": "newstaff",
+                "password1": "testpassword",
+                "password2": "testpassword",
             },
         )
-        self.assertEqual(response.status_code, 302)
-        self.assertTrue(
-            User.objects.filter(username=self.username, is_staff=True).exists()
-        )
 
-    def test_user_login(self):
-        User.objects.create_user(username=self.username, password=self.password)
+        self.assertEqual(
+            response.status_code, 302
+        )  # Check if the redirect is successful
+
+        # Check if the staff user was created and logged in
+        new_staff = User.objects.get(username="newstaff")
+        self.assertTrue(new_staff.is_authenticated)
+        self.assertTrue(new_staff.is_staff)
+
+    def test_user_log_sign_page_view(self):
         response = self.client.post(
-            reverse("userloginpage"), {"email": self.username, "pswd": self.password}
+            reverse("userloginpage"),
+            {
+                "email": self.test_user.username,
+                "pswd": "testpassword",
+            },
         )
-        self.assertEqual(response.status_code, 302)  # assuming it redirects to home
-        self.assertIn(
-            "_auth_user_id", self.client.session
-        )  # check if user is logged in
 
+        self.assertEqual(
+            response.status_code, 302
+        )  # Check if the redirect is successful
 
+        # Check if the user is logged in
+        self.assertTrue(response.wsgi_request.user.is_authenticated)
 
-    def test_staff_login(self):
-        User.objects.create_user(
-            username=self.username, password=self.password, is_staff=True
-        )
+    def test_logoutuser_view(self):
+        response = self.client.get(reverse("logout"))
+
+        self.assertEqual(
+            response.status_code, 302
+        )  # Check if the redirect is successful
+
+        # Check if the user is logged out
+        self.assertFalse(response.wsgi_request.user.is_authenticated)
+
+    def test_staff_log_sign_page_view(self):
         response = self.client.post(
             reverse("staffloginpage"),
-            {"username": self.username, "password": self.password},
+            {
+                "username": "testuser",
+                "password": "testpassword",
+            },
         )
+
         self.assertEqual(
             response.status_code, 302
-        )  # assuming it redirects to staff panel
-        self.assertIn("_auth_user_id", self.client.session)
+        )  # Check if the redirect is successful
 
-    def test_user_logout(self):
-        user = User.objects.create_user(username=self.username, password=self.password)
-        self.client.force_login(user)
-        response = self.client.get(reverse("logout"))
-        self.assertEqual(
-            response.status_code, 302
-        )  # assuming it redirects after logout
-        self.assertNotIn(
-            "_auth_user_id", self.client.session
-        )  # user should be logged out
-
-    def test_staff_log_sign_page(self):
-        response = self.client.get(reverse("staffloginpage"))
-        self.assertEqual(
-            response.status_code, 200
-        )  # should render the login/signup page for staff    
-
-        
+        # Check if the staff user is logged in
+        self.assertFalse(response.wsgi_request.user.is_authenticated)
+        self.assertFalse(response.wsgi_request.user.is_staff)
--- hotel/managment/tests.py
+++ hotel/managment/tests.py
@@ -1,50 +1,177 @@
-from django.test import TestCase
 from django.test import TestCase, Client
-from django.urls import reverse
 from django.contrib.auth.models import User
-from app.models import Hotels, Rooms
+from django.urls import reverse
+from app.models import Hotels, Rooms, Reservation
 
 
-class StaffViewTestCase(TestCase):
-    # ...[setUp and tearDown methods]...
+class ManagmentViewsTestCase(TestCase):
+    def setUp(self):
+        # Create a staff user
+        self.staff_user = User.objects.create_user(
+            username="staffuser", password="testpassword", is_staff=True
+        )
+        self.client = Client()
 
-    def test_add_new_location_with_staff_user(self):
+    def test_add_new_location_view(self):
+        # Log in the staff user
         self.client.login(username="staffuser", password="testpassword")
+
         response = self.client.post(
             reverse("addnewlocation"),
             {
-                "new_owner": "owner2",
-                "new_city": "city2",
-                "new_state": "state2",
-                "new_country": "country2",
+                "new_owner": "New Owner",
+                "new_city": "New City",
+                "new_state": "New State",
+                "new_country": "New Country",
             },
         )
-        self.assertEqual(response.status_code, 302)
-        self.assertTrue(Hotels.objects.filter(hotel_location="city2").exists())
 
+        self.assertEqual(
+            response.status_code, 302
+        )  # Check if the redirect is successful
 
+        # Check if the new location was added
+        new_hotel = Hotels.objects.get(
+            hotel_location="New City", hotel_state="New State"
+        )
+        self.assertEqual(new_hotel.hotel_owner, "New Owner")
 
-
-    def test_all_bookings_with_staff_user(self):
+    def test_all_bookings_view(self):
+        # Log in the staff user
         self.client.login(username="staffuser", password="testpassword")
+
         response = self.client.get(reverse("allbookigs"))
-        self.assertEqual(response.status_code, 200)
 
+        self.assertEqual(
+            response.status_code, 200
+        )  # Check if the view returns a successful response
 
+        # Add more assertions as needed based on the view's logic
 
+    def test_view_room_view(self):
+        # Create a room for testing
+        test_hotel = Hotels.objects.create(
+            hotel_name="Test Hotel",
+            hotel_owner="Test Owner",
+            hotel_location="Test City",
+            hotel_state="Test State",
+            hotel_country="Test Country",
+        )
+        test_room = Rooms.objects.create(
+            room_type="1",
+            max_capacity=2,
+            room_price=100,
+            room_size=20,
+            hotel=test_hotel,
+            availability_status="1",
+            room_number=101,
+        )
 
-
-    def test_view_room_with_staff_user(self):
+        # Log in the staff user
         self.client.login(username="staffuser", password="testpassword")
-        response = self.client.get(reverse("view_room"), {"roomid": self.room.id})
-        self.assertEqual(response.status_code, 200)
 
+        response = self.client.get(reverse("view_room") + f"?roomid={test_room.id}")
 
+        self.assertEqual(
+            response.status_code, 200
+        )  # Check if the view returns a successful response
 
+        # Add more assertions as needed based on the view's logic
 
-
-
-    def test_panel_with_staff_user(self):
+    def test_panel_view(self):
+        # Log in the staff user
         self.client.login(username="staffuser", password="testpassword")
+
         response = self.client.get(reverse("staffpanel"))
-        self.assertEqual(response.status_code, 200)
+
+        self.assertEqual(
+            response.status_code, 200
+        )  # Check if the view returns a successful response
+
+        # Add more assertions as needed based on the view's logic
+
+    def test_edit_room_view(self):
+        # Create a room for testing
+        test_hotel = Hotels.objects.create(
+            hotel_name="Test Hotel",
+            hotel_owner="Test Owner",
+            hotel_location="Test City",
+            hotel_state="Test State",
+            hotel_country="Test Country",
+        )
+        test_room = Rooms.objects.create(
+            room_type="1",
+            max_capacity=2,
+            room_price=100,
+            room_size=20,
+            hotel=test_hotel,
+            availability_status="1",
+            room_number=101,
+        )
+
+        # Log in the staff user
+        self.client.login(username="staffuser", password="testpassword")
+
+        response = self.client.post(
+            reverse("edit_room") + f"?roomid={test_room.id}",
+            {
+                "roomid": test_room.id,
+                "hotel": test_hotel.id,
+                "roomtype": "2",
+                "capacity": 3,
+                "price": 120,
+                "size": 25,
+                "status": "2",
+                "roomnumber": 102,
+            },
+        )
+
+        self.assertEqual(
+            response.status_code, 302
+        )  # Check if the redirect is successful
+
+        # Check if the room details were updated
+        updated_room = Rooms.objects.get(id=test_room.id)
+        self.assertEqual(updated_room.room_type, "2")
+        self.assertEqual(updated_room.max_capacity, 3)
+        self.assertEqual(updated_room.room_price, 120)
+        self.assertEqual(updated_room.room_size, 25)
+        self.assertEqual(updated_room.availability_status, "2")
+        self.assertEqual(updated_room.room_number, 102)
+
+    def test_add_new_room_view(self):
+        # Create a hotel for testing
+        test_hotel = Hotels.objects.create(
+            hotel_name="Test Hotel",
+            hotel_owner="Test Owner",
+            hotel_location="Test City",
+            hotel_state="Test State",
+            hotel_country="Test Country",
+        )
+
+        # Log in the staff user
+        self.client.login(username="staffuser", password="testpassword")
+
+        response = self.client.post(
+            reverse("addroom"),
+            {
+                "hotel": test_hotel.id,
+                "roomtype": "1",
+                "capacity": 2,
+                "price": 100,
+                "size": 20,
+                "status": "1",
+            },
+        )
+
+        self.assertEqual(
+            response.status_code, 302
+        )  # Check if the redirect is successful
+
+        # Check if the new room was added
+        new_room = Rooms.objects.get(hotel=test_hotel)
+        self.assertEqual(new_room.room_type, "1")
+        self.assertEqual(new_room.max_capacity, 2)
+        self.assertEqual(new_room.room_price, 100)
+        self.assertEqual(new_room.room_size, 20)
+        self.assertEqual(new_room.availability_status, "1")
